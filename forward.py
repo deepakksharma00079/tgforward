55e6ca95abfec350d1d3ea891ae8fbdd3da23105
deepakksharma00079 <97537662+deepakksharma00079@users.noreply.github.com>
2022-01-25 11:25:38 +0530

    Update forward.py

--- forward.py
+++ forward.py
@@ -53,7 +53,7 @@ async def forward():
     mode = InputMessagesFilterPhotos
   elif FILE_TYPE == "all":
     print("Now forwarding all messages")
-    mode = None
+    mode = InputMessageFilterEmpty()
   elif not FILE_TYPE:
     print("No file type given. System exiting...")
     sys.exit()
